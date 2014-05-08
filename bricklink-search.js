3f05321557e795f8d5c4a82db07a0de2140228a5
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-05-08 14:34:55 +0100

    Refactoring

--- bricklink-search.js
+++ bricklink-search.js
@@ -1,5 +1,4 @@
-var DEBUG = true,
-	SEARCH_PAGE_DEPTH = 25, // because of the BrickLink duplication bug, these are actually half of it!
+var SEARCH_PAGE_DEPTH = 25, // because of the BrickLink duplication bug, these are actually half of it!
 	MAX_SIMULTANEOUS_QUERIES = 3;
 
 var async = require('async'),
@@ -8,110 +7,120 @@ var async = require('async'),
 	request = require('request'),
 	_ = require('underscore');
 
-var log = function (message) {
-	if (DEBUG) {
-		var d = new Date();
-		console.log(d.getFullYear() + "/" + (d.getMonth() < 9 ? '0' : '') + (d.getMonth() + 1) + "/" + (d.getDate() < 10 ? '0' : '') + d.getDate() + " " + (d.getHours() < 10 ? '0' : '') + d.getHours() + ":" + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() + ":" + (d.getSeconds() < 10 ? '0' : '') + d.getSeconds() + " - " + message);
+module.exports = function (options) {
+
+	var log = function (message) {
+		if (options.debug) {
+			var d = new Date();
+			console.log(d.getFullYear() + "/" + (d.getMonth() < 9 ? '0' : '') + (d.getMonth() + 1) + "/" + (d.getDate() < 10 ? '0' : '') + d.getDate() + " " + (d.getHours() < 10 ? '0' : '') + d.getHours() + ":" + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() + ":" + (d.getSeconds() < 10 ? '0' : '') + d.getSeconds() + " - " + message);
+		}
+	};
+
+	var search = function (searchOptions, callback) {
+
+		var searchQueue = async.queue(function (searchOptions, callback) {
+			log("Searching for " + JSON.stringify(searchOptions));
+			request({
+					'url': "http://www.bricklink.com/searchAdvanced.asp",
+					'method': "POST",
+					'followAllRedirects': true,
+					'form': {
+						'a': "g",
+						'qMin': searchOptions.quantity,
+						'saleOff': "0",
+						'searchSort': "P", 
+						'sz': options.searchPageDepth || SEARCH_PAGE_DEPTH,
+						'q': searchOptions.partId,
+						'sellerUsername': searchOptions.sellerUsername,
+						'sellerLoc': "C",
+						'sellerCountryID': "UK",
+						'regionID': "-1",
+						'shipTo': "Y",
+						'shipCountryID': "UK",
+						'moneyTypeID': "27", // Pound Sterling
+						'pmtMethodID': "11", // PayPal
+					},
+				}, function (err, response, body) {
+					var results = [ ];
+					if (err || response.statusCode !== 200) {
+						callback(new Error("Could not read BrickLink results page."), [ ]);
+					} else {
+						var $ = cheerio.load(body);
+						results = $('tr.tm').map(function (index, elem) {
+							var item = { 
+								'partId': searchOptions.partId,
+								'new': "new" === $('td:nth-child(2)', elem).text().toLowerCase(),
+								// the code, for the time being, consider only UK
+								// sellers shipping to UK customers, so this is not
+								// relevant
+								// 'location': $('td:nth-child(3) font font', elem).text().match(/Loc: (.+),/)[1],
+								'minBuy': $('td:nth-child(3) font font', elem).text().match(/Min Buy: (.+)$/)[1],
+								'quantity': parseInt($(elem).text().match(/Qty: (.+)Each/)[1]),						
+								'price': parseFloat($('td:nth-child(4) font:nth-child(1)', elem).text().match(/Each\:.~GBP (.+)\(/)[1]),						
+								// 'sellerName': $('td:nth-child(4) a', elem).html(),
+								'sellerNoOfFeedback': parseInt($('td:nth-child(4) a', elem).text().match(/\((.+)\)/)),
+								'sellerUsername': qs.parse($('td:nth-child(4) a', elem).attr('href').match(/\?(.+)/)[1]).p,
+							};
+							if (item.sellerNoOfFeedback) item.sellerNoOfFeedback = item.sellerNoOfFeedback[1];
+							item.minBuy = item.minBuy === "None" ? null : parseFloat(item.minBuy.match(/~GBP (.+)/)[1]);
+							item.sellerUrl = "http://www.bricklink.com/store.asp?" + qs.stringify({ 'p': item.sellerUsername });
+							// TODO, rather than the URL, check the actual feedback!
+							item.sellerFeedbackUrl = "http://www.bricklink.com/feedback.asp?" + qs.stringify({ 'u': item.sellerUsername });
+							return item;
+						}).get();	
+						// this is made necessary because at the moment BrickLink is
+						// returning duplicate results!
+						results = _.uniq(results, false, function (x) { return x.sellerUsername + '_' + x.price; });
+						callback(null, results);
+					}
+			});
+		}, options.maxSimultaneousQueries || MAX_SIMULTANEOUS_QUERIES);
+
+		var searchPart = function (searchOptions, callback) {
+			searchQueue.push(searchOptions, callback);
+		}
+
+		searchOptions = [ ].concat(JSON.parse(JSON.stringify(searchOptions)) || [ ]);
+		var firstRoundResults = [ ],
+			secondRoundResults = [ ];
+		// first round, search for sellers that can provide the full number of 
+		// pieces for at least one part
+		log("Start of first round of search...");
+		async.each(searchOptions, function (s, callback) {
+			searchPart(s, function (err, results) {
+				firstRoundResults = firstRoundResults.concat(results);
+				callback(err);
+			});
+		}, function (err) {
+			// second round, for all sellers identified this far, search also for 
+			// partial availability of all other parts
+			log("Start of second round of search...");
+			var allPartIds = searchOptions.map(function (s) { return s.partId; });
+			var sellerUsernames = _.uniq(firstRoundResults.map(function (r) { return r.sellerUsername; })).sort();
+			async.each(sellerUsernames, function (sellerUsername, callback) {
+				var fullAvailabilityPartIds = firstRoundResults.reduce(function (memo, r) { 
+						if (r.sellerUsername === sellerUsername) memo = memo.concat(r.partId);
+						return memo; 
+					}, [ ]);
+				async.each(_.difference(allPartIds, fullAvailabilityPartIds), function (partId, callback) {
+					searchPart({ 'partId': partId, 'sellerUsername': sellerUsername }, function (err, results) {
+						if (results.length > 0) {
+							log("Adding partial availability of " + partId + " from " + sellerUsername + ".");
+							secondRoundResults = secondRoundResults.concat(results);
+						}
+						callback(err);
+					});				
+				}, callback);
+			}, function (err) {
+				callback(err, firstRoundResults.concat(secondRoundResults));
+			});
+		});
+	};
+
+	return {
+		'search': search,
 	}
+
 }
 
-exports.search = function (searchOptions, callback) {
 
-	var searchQueue = async.queue(function (searchOptions, callback) {
-		log("Searching for " + JSON.stringify(searchOptions));
-		request({
-				'url': "http://www.bricklink.com/searchAdvanced.asp",
-				'method': "POST",
-				'followAllRedirects': true,
-				'form': {
-					'a': "g",
-					'qMin': searchOptions.quantity,
-					'saleOff': "0",
-					'searchSort': "P", 
-					'sz': SEARCH_PAGE_DEPTH,
-					'q': searchOptions.partId,
-					'sellerUsername': searchOptions.sellerUsername,
-					'sellerLoc': "C",
-					'sellerCountryID': "UK",
-					'regionID': "-1",
-					'shipTo': "Y",
-					'shipCountryID': "UK",
-					'moneyTypeID': "27", // Pound Sterling
-					'pmtMethodID': "11", // PayPal
-				},
-			}, function (err, response, body) {
-				var results = [ ];
-				if (err || response.statusCode !== 200) {
-					callback(new Error("Could not read BrickLink results page."), [ ]);
-				} else {
-					var $ = cheerio.load(body);
-					results = $('tr.tm').map(function (index, elem) {
-						var item = { 
-							'partId': searchOptions.partId,
-							'new': "new" === $('td:nth-child(2)', elem).text().toLowerCase(),
-							// the code, for the time being, consider only UK
-							// sellers shipping to UK customers, so this is not
-							// relevant
-							// 'location': $('td:nth-child(3) font font', elem).text().match(/Loc: (.+),/)[1],
-							'minBuy': $('td:nth-child(3) font font', elem).text().match(/Min Buy: (.+)$/)[1],
-							'quantity': parseInt($(elem).text().match(/Qty: (.+)Each/)[1]),						
-							'price': parseFloat($('td:nth-child(4) font:nth-child(1)', elem).text().match(/Each\:.~GBP (.+)\(/)[1]),						
-							// 'sellerName': $('td:nth-child(4) a', elem).html(),
-							'sellerNoOfFeedback': parseInt($('td:nth-child(4) a', elem).text().match(/\((.+)\)/)),
-							'sellerUsername': qs.parse($('td:nth-child(4) a', elem).attr('href').match(/\?(.+)/)[1]).p,
-						};
-						if (item.sellerNoOfFeedback) item.sellerNoOfFeedback = item.sellerNoOfFeedback[1];
-						item.minBuy = item.minBuy === "None" ? null : parseFloat(item.minBuy.match(/~GBP (.+)/)[1]);
-						item.sellerUrl = "http://www.bricklink.com/store.asp?" + qs.stringify({ 'p': item.sellerUsername });
-						// TODO, rather than the URL, check the actual feedback!
-						item.sellerFeedbackUrl = "http://www.bricklink.com/feedback.asp?" + qs.stringify({ 'u': item.sellerUsername });
-						return item;
-					}).get();	
-					// this is made necessary because at the moment BrickLink is
-					// returning duplicate results!
-					results = _.uniq(results, false, function (x) { return x.sellerUsername + '_' + x.price; });
-					callback(null, results);
-				}
-		});
-	}, MAX_SIMULTANEOUS_QUERIES);
-
-	var searchPart = function (searchOptions, callback) {
-		searchQueue.push(searchOptions, callback);
-	}
-
-	searchOptions = [ ].concat(JSON.parse(JSON.stringify(searchOptions)) || [ ]);
-	var firstRoundResults = [ ],
-		secondRoundResults = [ ];
-	// first round, search for sellers that can provide the full number of 
-	// pieces for at least one part
-	log("Start of first round of search...");
-	async.each(searchOptions, function (s, callback) {
-		searchPart(s, function (err, results) {
-			firstRoundResults = firstRoundResults.concat(results);
-			callback(err);
-		});
-	}, function (err) {
-		// second round, for all sellers identified this far, search also for 
-		// partial availability of all other parts
-		log("Start of second round of search...");
-		var allPartIds = searchOptions.map(function (s) { return s.partId; });
-		var sellerUsernames = _.uniq(firstRoundResults.map(function (r) { return r.sellerUsername; })).sort();
-		async.each(sellerUsernames, function (sellerUsername, callback) {
-			var fullAvailabilityPartIds = firstRoundResults.reduce(function (memo, r) { 
-					if (r.sellerUsername === sellerUsername) memo = memo.concat(r.partId);
-					return memo; 
-				}, [ ]);
-			async.each(_.difference(allPartIds, fullAvailabilityPartIds), function (partId, callback) {
-				searchPart({ 'partId': partId, 'sellerUsername': sellerUsername }, function (err, results) {
-					if (results.length > 0) {
-						log("Adding partial availability of " + partId + " from " + sellerUsername + ".");
-						secondRoundResults = secondRoundResults.concat(results);
-					}
-					callback(err);
-				});				
-			}, callback);
-		}, function (err) {
-			callback(err, firstRoundResults.concat(secondRoundResults));
-		});
-	});
-};
--- main.js
+++ main.js
@@ -1,168 +1,38 @@
 var argv = require('yargs')
-		.demand([ 'out '])
+		.demand([ 'in', 'out' ])
 		.argv,
 	async = require('async'),
+	csv = require('csv'),
 	fs = require('fs'),
 	path = require('path'),
 	_ = require('underscore'),
-	bricklinkSearch = require('./bricklink-search');
+	bricklinkSearch = new require('./bricklink-search')({ 'debug': true }),
+	bricklinkOrder = new require('./bricklink-order')();
 
-var PARTS_LIST = [
-	// page 1
-	{ 'partId': "235726", 'quantity': 4 }, { 'partId': "4560182", 'quantity': 3 },
-	{ 'partId': "4180536", 'quantity': 2 }, { 'partId': "302326", 'quantity': 9 },
-	{ 'partId': "379426", 'quantity': 3 }, { 'partId': "379526", 'quantity': 2 },
-	{ 'partId': "241226", 'quantity': 15 }, { 'partId': "4180508", 'quantity': 2 },
-	{ 'partId': "329826", 'quantity': 1 }, { 'partId': "374726", 'quantity': 1 },
-	{ 'partId': "4528331", 'quantity': 3 },	{ 'partId': "4118793", 'quantity': 2 },
-	{ 'partId': "4113915", 'quantity': 4 },	
-	// page 2
-	{ 'partId': "4109995", 'quantity': 2 }, { 'partId': "4162465", 'quantity': 6 },	
-	{ 'partId': "4113916", 'quantity': 16 }, { 'partId': "4112982", 'quantity': 11 }, 
-	{ 'partId': "4114319", 'quantity': 2 }, { 'partId': "4181134", 'quantity': 2 },	
-	{ 'partId': "4579306", 'quantity': 2 }, { 'partId': "4114077", 'quantity': 1 }, 
-	{ 'partId': "4114026", 'quantity': 2 }, { 'partId': "4550324", 'quantity': 10 },
-	{ 'partId': "4157277", 'quantity': 11 }, { 'partId': "4544139", 'quantity': 1 },
-	{ 'partId': "4185177", 'quantity': 3 },	
-	// page 3
-	{ 'partId': "4113917", 'quantity': 7 }, { 'partId': "4155708", 'quantity': 9 },	
-	{ 'partId': "4124067", 'quantity': 4 }, { 'partId': "4114324", 'quantity': 2 },	
-	{ 'partId': "4565387", 'quantity': 6 }, { 'partId': "4118790", 'quantity': 6 },	
-	{ 'partId': "4113993", 'quantity': 2 }, { 'partId': "4113988", 'quantity': 2 },	
-	{ 'partId': "4243824", 'quantity': 2 }, { 'partId': "4509897", 'quantity': 4 },	
-	{ 'partId': "4655900", 'quantity': 6 }, { 'partId': "6069165", 'quantity': 4 },	
-	{ 'partId': "4536667", 'quantity': 1 },
-	// page 4
-	{ 'partId': "307021", 'quantity': 1 }, { 'partId': "4626001", 'quantity': 1 },
-	{ 'partId': "6000606", 'quantity': 5 },	{ 'partId': "4211052", 'quantity': 4 },
-	{ 'partId': "4210998", 'quantity': 1 }, { 'partId': "4211043", 'quantity': 4 },
-	{ 'partId': "4278274", 'quantity': 4 }, { 'partId': "4654582", 'quantity': 2 },
-	{ 'partId': "4650260", 'quantity': 7 }, { 'partId': "4211397", 'quantity': 2 },
-	{ 'partId': "4622803", 'quantity': 5 }, 
-];
-
-/*
-bricklinkSearch.search(PARTS_LIST, function (err, byPart) {
-	fs.writeFileSync(path.join(__dirname, "foo.json"), JSON.stringify(byPart));
-	console.log("Finished.");
-});
-*/
-
-var data = JSON.parse(fs.readFileSync(path.join(__dirname, "test-data.json")));
-
-// Returns the list of bricks specified by the part ids in the partIds 
-// array the seller can provide. If partIds is not specified, it returns all
-// possible part ids. If more prices are available for the same part ids, the
-// cheapest options are favoured.
-var getListOfPiecesSellerCanOffer = function (requirement, availability, sellerUsername, partIds) {
-	// we need _.uniq below because the same seller may have the same part 
-	// in different prices
-	partIds = _.uniq(partIds ? [ ].concat(partIds) : availability.reduce(function (memo, a) {
-		if (a.sellerUsername === sellerUsername) memo = memo.concat(a.partId);
-		return memo;
-	}, [ ]));
-	return partIds.reduce(function (memo, partId) {
-		// the number of bricks I need
-		var totalRequirement = (_.find(requirement, function (p) {
-				return p.partId === partId;
-			}) || { 'quantity': 0 }).quantity;
-		// the seller's availability
-		var sellerAvailability = availability
-			.filter(function (a) {
-				return (a.sellerUsername === sellerUsername) && (a.partId === partId);
-			})
-			.sort(function (a, b) {
-				return a.price - b.price;
-			})
-			.reduce(function (memo, a) {
-				var missing = totalRequirement - memo.reduce(function (sum, i) { return sum + i.quantity; }, 0);
-				if (missing > 0) {
-					var newItems = JSON.parse(JSON.stringify(a));
-					newItems.quantity = Math.min(missing, newItems.quantity);
-					memo = memo.concat(newItems);					
-				}
-				return memo;
-			}, [ ]);
-		return memo.concat(sellerAvailability);
-	}, [ ]);
-};
-
-// Returns the max number of bricks specified by the part ids in the partIds 
-// array the seller can provide. If partIds is not specified, it returns all
-// possible part ids.
-var getMaxNoOfPiecesSellerCanOffer = function (requirement, availability, sellerUsername, partIds) {
-	return getListOfPiecesSellerCanOffer(requirement, availability, sellerUsername, partIds).reduce(function (memo, part) {
-			return memo + part.quantity;
-		}, 0);
-};
-
-var getSellerUsernamesBySizeOfPotentialOrder = function (requirement, availability) {
-	return _.uniq(availability.map(function (a) { return a.sellerUsername; }))
-		.reduce(function (memo, sellerUsername) {
-			var item = { 
-				'username': sellerUsername, 
-				'count': getMaxNoOfPiecesSellerCanOffer(requirement, availability, sellerUsername)
-			};
-			if (item.count > 0) memo = memo.concat(item);
-			return memo;
-		}, [ ])
-		.sort(function (a, b) {
-			return b.count - a.count;
-		})
-		.map(function (x) { return x.username; });
-};
-
-var makeOrder = function (requirement, availability, sellerUsername) {
-
-	var makeOrderFromSeller = function (requirement, availability, sellerUsername) {
-		var pieces = getListOfPiecesSellerCanOffer(requirement, availability, sellerUsername);
-		var newRequirement = JSON.parse(JSON.stringify(requirement));
-		var newAvailability = JSON.parse(JSON.stringify(availability));
-		// I decrease the requirments by the volume of pieces I am ordering from
-		// the seller
-		newRequirement = newRequirement.reduce(function (memo, r) {
-			pieces
-				.filter(function (p) {
-					return r.partId === p.partId;
-				})
-				.forEach(function (p) {
-					r.quantity -= p.quantity;
-				});
-			if (r.quantity > 0) memo = memo.concat(r);
-			return memo;
-		}, [ ]);
-		// I remove the seller from the availability
-		newAvailability = newAvailability.filter(function (a) {
-			return a.sellerUsername !== sellerUsername;
+var readPartsList = function (filename, callback) {
+	csv()
+		.from.path(filename, { 'columns': true })
+		.to.array(function (data) {
+			callback(null, data);
 		});
-		return {
-			'pieces': pieces,
-			'newRequirement': newRequirement,
-			'newAvailability': newAvailability,
-		};
+};
+
+readPartsList(argv.in, function (err, partsList) {
+
+	function makeOrder() {
+		fs.writeFileSync(argv.out, JSON.stringify(bricklinkOrder.makeOrder(partsList, availability)));
+		console.log("Finished.");
 	}
 
-	if (sellerUsername) {
-		return makeOrderFromSeller(requirement, availability, sellerUsername);
+	var availability;
+	if (argv.cache && fs.existsSync(argv.cache)) {
+		availability = JSON.parse(fs.readFileSync(argv.cache));
+		makeOrder();
 	} else {
-		var newRequirement = JSON.parse(JSON.stringify(requirement));
-		var newAvailability = JSON.parse(JSON.stringify(availability));
-		var orders = { };
-		var keepSearching = true;
-		while (keepSearching && (newRequirement.reduce(function (memo, r) { return memo + r.quantity; }, 0) > 0)) {
-			var topSellerUsername = _.first(getSellerUsernamesBySizeOfPotentialOrder(newRequirement, newAvailability));
-			keepSearching = topSellerUsername;
-			if (topSellerUsername) {
-				var temp = makeOrderFromSeller(newRequirement, newAvailability, topSellerUsername);
-				orders[topSellerUsername] = temp.pieces;
-				newRequirement = temp.newRequirement;
-				newAvailability = temp.newAvailability;			
-			} else {
-				console.log("Could not find sellers for " + newRequirement.map(function (r) { return r.partId; }));
-			}
-		}
-		return orders;
+		bricklinkSearch.search(partsList, function (err, a) {
+			availability = a;
+			fs.writeFileSync(argv.cache, JSON.stringify(availability));
+			makeOrder();
+		});
 	}
-}
-
-fs.writeFileSync(argv.out, JSON.stringify(makeOrder(PARTS_LIST, data)));
+});
